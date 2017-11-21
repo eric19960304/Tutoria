1786eefa0d13f50c442c96b9b51313cc1fcc17c3
Eric Lau <ericlauchiho@gmail.com>
2017-11-21 14:55:23 +0800

    improve scheduler

--- scheduler.py
+++ scheduler.py
@@ -2,12 +2,16 @@ import schedule
 import time
 import os 
 
+def job_placer():
+    print("Places the job:")
+    schedule.every(30).minutes.do(job)
+    return schedule.CancelJob # only cancel the job_placer() routine and will not affect the sheduled job()
 
 def job():
     os.system('python manage.py managesession >> manage_session_log.txt')
 
+schedule.every().day.at("01:59").do(job_placer)
 #schedule.every().seconds.do(job)
-schedule.every(30).minutes.do(job)
 #schedule.every().hour.do(job)
 #schedule.every().day.at("12:53").do(job)
 #schedule.every().monday.do(job)
